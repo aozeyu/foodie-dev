a5dd330454aa2a3988a96be0955180f4e8b3acde
yaozeyu3 <yaozeyu3@jd.com>
2022-04-08 10:48:29 +0800

    添加接口

--- foodie-dev-service/src/main/java/com/imooc/service/iml/StuServiceIml.java
+++ foodie-dev-service/src/main/java/com/imooc/service/iml/StuServiceIml.java
@@ -5,16 +5,18 @@ import com.imooc.pojo.Stu;
 import com.imooc.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StuServiceIml implements StuService {
 //引入凑想方法
     @Autowired
     private StuMapper stuMapper;
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public Stu getStuInfo(int id) {
-
-        return null;
+        return stuMapper.selectByPrimaryKey(id);
     }
 
     @Override
