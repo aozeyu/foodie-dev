fe11f11e905d12be0a31e6b3ddf350f96e5c9757
yaozeyu3 <yaozeyu3@jd.com>
2022-04-08 14:36:01 +0800

    添加更新删除的接口

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -1,9 +1,9 @@
 package com.imooc;
 
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.imooc.mapper")
--- foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
@@ -3,6 +3,8 @@ package com.imooc.controller;
 import com.imooc.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +16,20 @@ public class StuFooController {
     public Object getStu(int id) {
 
         return stuService.getStuInfo(id);
+    }
+    @PostMapping("/saveStu")
+    public Object saveStu() {
+        stuService.saveStu();
+        return "OK";
+    }
+    @PutMapping("/updateStu")
+    public Object updateStu(int id) {
+        stuService.updateStu(id);
+        return "OK";
+    }
+    @PutMapping("/deleteStu")
+    public Object deleteStul(int id){
+        stuService.deleteStu(id);
+        return "OK";
     }
 }
